d8dbc150d362d2397fcfaadd8fed6b2e1ca00d71
Arthur <mlkliso4000@gmail.com>
2025-09-01 09:36:04 -0300

    animais

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import dados from "./src/data/dados.js";
-const { bruxos, varinhas, pocoes } = dados
+const { bruxos, varinhas, pocoes, animais } = dados;
 // Criar aplicação com Express e configurar para aceitar JSON
 const app = express();
 app.use(express.json());
@@ -14,121 +14,230 @@ const serverPort = process.env.PORT || 3001;
 
 // Rota principal GET para "/"
 app.get("/", (req, res) => {
-    res.send("🚀 Servidor funcionando...");
+  res.send("🚀 Servidor funcionando...");
 });
-
 
 // Aqui vão todas suas Rotas
 // Query Parameters no Node.js - API de Hogwarts
-app.get('/bruxos', (req, res) => {
-    const { casa, ano, especialidade, nome } = req.query;
-    let resultado = bruxos;
-  
-    if (casa) {
-      resultado = resultado.filter(b => b.casa.toLowerCase() === casa.toLowerCase());
-    }
-  
-    if (ano) {
-      resultado = resultado.filter(b => b.ano == ano);
-    }
-  
-    if (especialidade) {
-      resultado = resultado.filter(b => b.especialidade.toLowerCase().includes(especialidade.toLowerCase()));
-    }
-  
-    if (nome) {
-      resultado = resultado.filter(b => b.nome.toLowerCase().includes(nome.toLowerCase()));
-    }
-  
-    res.status(200).json({
-      total: resultado.length,
-      data: resultado
-    });
+app.get("/bruxos", (req, res) => {
+  const { casa, ano, especialidade, nome } = req.query;
+  let resultado = bruxos;
+
+  if (casa) {
+    resultado = resultado.filter(
+      (b) => b.casa.toLowerCase() === casa.toLowerCase()
+    );
+  }
+
+  if (ano) {
+    resultado = resultado.filter((b) => b.ano == ano);
+  }
+
+  if (especialidade) {
+    resultado = resultado.filter((b) =>
+      b.especialidade.toLowerCase().includes(especialidade.toLowerCase())
+    );
+  }
+
+  if (nome) {
+    resultado = resultado.filter((b) =>
+      b.nome.toLowerCase().includes(nome.toLowerCase())
+    );
+  }
+
+  res.status(200).json({
+    total: resultado.length,
+    data: resultado,
+  });
+});
+
+app.get("/stats", (req, res) => {
+  const { casa } = req.query;
+  let resultado = bruxos;
+
+  if (casa) {
+    resultado = resultado.filter(
+      (b) => b.casa.toLowerCase() === casa.toLowerCase()
+    );
+  }
+
+  res.status(200).json({
+    Bruxos: `${casa} = ${resultado.length}`,
+  });
 });
 
 // Body Parameters no Node.js - Criar novo bruxo
 
-app.post('/bruxos', (req, res) => {
-    // Acessando dados do body
-    const { nome, casa, ano, varinha, mascote, patrono, especialidade, vivo } = req.body;
-    
-    console.log('Dados recebidos:', req.body);
-    
-    // Validação básica
-    if (!nome || !casa || !ano || !vivo) {
-        return res.status(400).json({
-            success: false,
-            message: "Nome, casa, ano e estar vivo são obrigatórios para um bruxo!"
-        });
-    }
-    
-    // Criar novo bruxo
-    const novoBruxo = {
-        id: bruxos.length + 1,
-        nome,
-        casa: casa,
-        ano: parseInt(ano),
-        varinha: varinha || "Ainda não definida",
-        mascote: mascote || "Ainda não definido",
-        patrono: patrono || "Ainda não descoberto",
-        especialidade: especialidade || "Em desenvolvimento",
-        vivo: vivo
-    };
-    
-    // Adicionar à lista de bruxos
-    bruxos.push(novoBruxo);
-    
-    res.status(201).json({
-        success: true,
-        message: "Novo bruxo adicionado a Hogwarts!",
-        data: novoBruxo
+app.post("/bruxos", (req, res) => {
+  // Acessando dados do body
+  const { nome, casa, ano, varinha, mascote, patrono, especialidade, vivo } =
+    req.body;
+
+  console.log("Dados recebidos:", req.body);
+
+  // Validação básica
+  if (!nome || !casa || !ano || !vivo) {
+    return res.status(400).json({
+      success: false,
+      message: "Nome, casa, ano e estar vivo são obrigatórios para um bruxo!",
     });
+  }
+
+  // Criar novo bruxo
+  const novoBruxo = {
+    id: bruxos.length + 1,
+    nome,
+    casa: casa,
+    ano: parseInt(ano),
+    varinha: varinha || "Ainda não definida",
+    mascote: mascote || "Ainda não definido",
+    patrono: patrono || "Ainda não descoberto",
+    especialidade: especialidade || "Em desenvolvimento",
+    vivo: vivo,
+  };
+
+  // Adicionar à lista de bruxos
+  bruxos.push(novoBruxo);
+
+  res.status(201).json({
+    success: true,
+    message: "Novo bruxo adicionado a Hogwarts!",
+    data: novoBruxo,
+  });
 });
 
-app.get('/varinhas', (req, res) => {
+app.get("/varinhas", (req, res) => {
   const { material, nucleo, comprimento } = req.query;
   let resultado = varinhas;
 
   if (material) {
-    resultado = resultado.filter(v => v.material.toLowerCase() === material.toLowerCase());
+    resultado = resultado.filter(
+      (v) => v.material.toLowerCase() === material.toLowerCase()
+    );
   }
 
   if (nucleo) {
-    resultado = resultado.filter(v => v.nucleo == nucleo);
+    resultado = resultado.filter((v) => v.nucleo == nucleo);
   }
 
   if (comprimento) {
-    resultado = resultado.filter(v => v.comprimento.toLowerCase().includes(especialidade.toLowerCase()));
+    resultado = resultado.filter((v) =>
+      v.comprimento.toLowerCase().includes(especialidade.toLowerCase())
+    );
   }
   res.status(200).json({
     total: resultado.length,
-    data: resultado
+    data: resultado,
   });
 });
 
-app.get('/pocoes', (req, res) => {
+app.post("/varinhas", (req, res) => {
+  // Acessando dados do body
+  const { material, nucleo, comprimento } = req.body;
+
+  console.log("Dados recebidos:", req.body);
+
+  // Validação básica
+  if (!material || !nucleo || !comprimento) {
+    return res.status(400).json({
+      success: false,
+      message:
+        "MAterial, núcleo e comprimento são necessários para criar uma nova varinha!",
+    });
+  }
+
+  // Criar novo bruxo
+  const novaVarinha = {
+    id: varinhas.length + 1,
+    material: material || "Ainda não definido",
+    nucleo: nucleo || "Ainda não definido",
+    comprimento: comprimento || "Ainda não descoberto",
+  };
+
+  // Adicionar à lista de bruxos
+  bruxos.push(novaVarinha);
+
+  res.status(201).json({
+    success: true,
+    message: "Novo bruxo adicionado a Hogwarts!",
+    data: novaVarinha,
+  });
+});
+
+app.get("/pocoes", (req, res) => {
   const { nome, efeito } = req.query;
   let resultado = pocoes;
 
   if (nome) {
-    resultado = resultado.filter(p => p.nome.toLowerCase() === nome.toLowerCase());
+    resultado = resultado.filter(
+      (p) => p.nome.toLowerCase() === nome.toLowerCase()
+    );
   }
 
   if (efeito) {
-    resultado = resultado.filter(p => p.efeito == efeito);
+    resultado = resultado.filter((p) => p.efeito == efeito);
   }
 
   res.status(200).json({
     total: resultado.length,
-    data: resultado
+    data: resultado,
+  });
+});
+
+app.post("/varinhas", (req, res) => {
+  // Acessando dados do body
+  const { material, nucleo, comprimento } = req.body;
+
+  console.log("Dados recebidos:", req.body);
+
+  // Validação básica
+  if (!material || !nucleo || !comprimento) {
+    return res.status(400).json({
+      success: false,
+      message:
+        "Material, núcleo e comprimento são necessários para criar uma nova varinha!",
+    });
+  }
+
+  // Criar novo bruxo
+  const novaVarinha = {
+    id: varinhas.length + 1,
+    material: material || "Ainda não definido",
+    nucleo: nucleo || "Ainda não definido",
+    comprimento: comprimento || "Ainda não descoberto",
+  };
+
+  // Adicionar à lista de bruxos
+  bruxos.push(novaVarinha);
+
+  res.status(201).json({
+    success: true,
+    message: "Novo bruxo adicionado a Hogwarts!",
+    data: novaVarinha,
+  });
+});
+
+app.get("/animais", (req, res) => {
+  const { nome, tipo } = req.query;
+  let resultado = animais;
+
+  if (nome) {
+    resultado = resultado.filter(
+      (a) => a.nome.toLowerCase() === nome.toLowerCase()
+    );
+  }
+
+  if (tipo) {
+    resultado = resultado.filter((a) => a.tipo == tipo);
+  }
+
+  res.status(200).json({
+    total: resultado.length,
+    data: resultado,
   });
 });
 
 // Iniciar servidor escutando na porta definida
 app.listen(serverPort, () => {
-    console.log(`🚀 Servidor rodando em http://localhost:${serverPort} 🚀`);
+  console.log(`🚀 Servidor rodando em http://localhost:${serverPort} 🚀`);
 });
-
-
-
-
